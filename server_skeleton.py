2856f4b97dedc5e73894dabb64abe7118c03b67b
Michał Szmuksta <szmuksta@student.agh.edu.pl>
2021-11-05 18:10:33 +0100

    Powolutku do przodu xd - drobne zmiany

--- server_skeleton.py
+++ server_skeleton.py
@@ -3,13 +3,20 @@
 
 from typing import Optional
 from typing import List, Dict
-
+import re
 
 class Product:
     # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą argumenty wyrażające nazwę produktu (typu str) i jego cenę (typu float) -- w takiej kolejności -- i ustawiającą atrybuty `name` (typu str) oraz `price` (typu float)
     def __init__(self, name: str, price: float):
+        if isinstance(price, int):
+            price = float(price)
+        if not (isinstance(name, str) and isinstance(price, float)) or not name.isalnum():
+            raise ValueError
+        if re.fullmatch('[a-zA-Z][0-9]', name) is None: # FIXME: No nie działa mi to na razie :(((((((((((((((
+            raise ValueError
         self.name: str = name
         self.price: float = price
+
     def __eq__(self, other):
         return self.name == other.name and self.price == other.price  # FIXME: zwróć odpowiednią wartość
 
@@ -17,7 +24,7 @@ class Product:
         return hash((self.name, self.price))
 
 
-class TooManyProductsFoundError:
+class TooManyProductsFoundError(Exception):
     # Reprezentuje wyjątek związany ze znalezieniem zbyt dużej liczby produktów.
     pass
 
@@ -28,35 +35,37 @@ class TooManyProductsFoundError:
 #   (3) możliwość odwołania się do metody `get_entries(self, n_letters)` zwracającą listę produktów spełniających kryterium wyszukiwania
 
 class ListServer:
-    def __init__(self, _products: List):
-        self._products = _products
+    def __init__(self, products: List[Product]):
+        self.products = products
 
     pass
 
 
 class MapServer:
-    def __init__(self, _products: Dict):
-        self._products = _products
+    def __init__(self, products: List[Product]):
+        self.products = {product.name: product.price for product in products}
 
     pass
 
 
 class Client:
     # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą obiekt reprezentujący serwer
-    def __init__(self, _server_id) -> None:
-        self._server_id = _server_id
+    def __init__(self, server_id) -> None:
+        self.server_id = server_id
 
     def get_total_price(self, n_letters: Optional[int]) -> Optional[float]:
-        try
+        try:
+            pass
+        except:
+            # TooManyProductsFoundError
+            pass
 
-            return
-        except
-            TooManyProductsFoundError
-            return None
     pass
+
+
 class Server:
     def __init__(self, id: int, n_max_returned_entries: int):
         self.id = id
         self.n_max_returned_entries = n_max_returned_entries
-    pass
 
+    pass
