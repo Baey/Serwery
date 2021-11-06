d54084eac8a98d83fe5d12a3a6c122e4ddbecc7c
Baey <blazejszargut@gmail.com>
2021-11-06 23:17:32 +0100

    Produkt gotowy, wyszukiwanie w serwerach tez

--- server_skeleton.py
+++ server_skeleton.py
@@ -3,22 +3,44 @@
 
 from typing import Optional
 from typing import List, Dict
-import re
+from abc import ABC, abstractmethod, abstractproperty
+
 
 class Product:
     # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą argumenty wyrażające nazwę produktu (typu str) i jego cenę (typu float) -- w takiej kolejności -- i ustawiającą atrybuty `name` (typu str) oraz `price` (typu float)
     def __init__(self, name: str, price: float):
-        if isinstance(price, int):
-            price = float(price)
-        if not (isinstance(name, str) and isinstance(price, float)) or not name.isalnum():
+
+        # Sprawdzamy zgodność typów
+        if not isinstance(name, str) or not isinstance(price, (float, int)):
             raise ValueError
-        if re.fullmatch('[a-zA-Z][0-9]', name) is None: # FIXME: No nie działa mi to na razie :(((((((((((((((
+
+        # Nie wiem co to było, więc zostawiam na razie
+        # if re.fullmatch('[a-zA-Z][0-9]', name) is None: # FIXME: No nie działa mi to na razie :(((((((((((((((
+        #     raise ValueError
+
+        # Tutaj bardzo 'pythonic' mi wyszło sprawdzenie czy ma co najmniej jeden znak i cyfrę
+        contains_number = True if True in [sign.isdigit() for sign in name] else False
+        contains_alpha = True if True in [sign.isalpha() for sign in name] else False
+        if not contains_alpha or not contains_number:
             raise ValueError
+
         self.name: str = name
-        self.price: float = price
+        self.price: float = float(price)
 
     def __eq__(self, other):
         return self.name == other.name and self.price == other.price  # FIXME: zwróć odpowiednią wartość
+
+    def __lt__(self, other):
+        return self.price < other.price
+
+    def __le__(self, other):
+        return self.price <= other.price
+
+    def __gt__(self, other):
+        return self.price > other.price
+
+    def __ge__(self, other):
+        return self.price >= other.price
 
     def __hash__(self):
         return hash((self.name, self.price))
@@ -34,18 +56,60 @@ class TooManyProductsFoundError(Exception):
 #   (2) możliwość odwołania się do atrybutu klasowego `n_max_returned_entries` (typu int) wyrażający maksymalną dopuszczalną liczbę wyników wyszukiwania,
 #   (3) możliwość odwołania się do metody `get_entries(self, n_letters)` zwracającą listę produktów spełniających kryterium wyszukiwania
 
-class ListServer:
-    def __init__(self, products: List[Product]):
-        self.products = products
+class Server(ABC):
+    n_max_returned_entries: int = 5
+    # ilość serwerów to zmienna dostępna dla każdej instancji klasy, tak jak n_max_returned_entries
+    # i jest wykorzystywana do przydzielania id do nowego serwera
+    servers_number = 0
 
-    pass
+    def __init__(self):
+        self.id = self.__class__.servers_number
+        self.__class__.servers_number += 1  # po powstaniu instancji klasy inkrementujemy zmienną klasową
+
+    @abstractmethod
+    def search_for_products(self, n_letters: int = 1) -> List[Product]:
+        pass
+
+
+class ListServer(Server):
+    def __init__(self, products: List[Product]):
+        super().__init__()  # Przy konstrukcji ListServer Tworzymy też Server, który jest klasą macierzystą
+        self.products: List[Product] = products
+
+    def search_for_products(self, n_letters: int = 1) -> List[Product]:
+        search_results: List[Product] = []
+        for product in self.products:
+            letters_counter = 0
+            numbers_counter = 0
+            for sign in product.name:
+                if sign.isalpha():
+                    letters_counter += 1
+                if sign.isdigit():
+                    numbers_counter += 1
+            if letters_counter == n_letters and numbers_counter in [2, 3]:
+                search_results.append(product)
+        return qsort_products(search_results)
 
 
 class MapServer:
     def __init__(self, products: List[Product]):
-        self.products = {product.name: product.price for product in products}
+        # self.products = {product.name: product.price for product in products}
+        # W treści zadania jest, że nazwa to klucz a wartość obiekt Product nie??
+        self.products = {product.name: product for product in products}
 
-    pass
+    def search_for_products(self, n_letters: int = 1) -> List[Product]:
+        search_results: List[Product] = []
+        for product in self.products:
+            letters_counter = 0
+            numbers_counter = 0
+            for sign in product:
+                if sign.isalpha():
+                    letters_counter += 1
+                if sign.isdigit():
+                    numbers_counter += 1
+            if letters_counter == n_letters and numbers_counter in [2, 3]:
+                search_results.append(self.products.get(product))
+        return qsort_products(search_results)
 
 
 class Client:
@@ -63,9 +127,23 @@ class Client:
     pass
 
 
-class Server:
-    def __init__(self, id: int, n_max_returned_entries: int):
-        self.id = id
-        self.n_max_returned_entries = n_max_returned_entries
-
-    pass
+def qsort_products(products: List[Product], start: int = 0, stop: int = -1) -> List[Product]:
+    if stop == -1:
+        stop = len(products) - 1
+    i = start
+    j = stop
+    pivot = products[start]
+    while i < j:
+        while products[i] < pivot:
+            i += 1
+        while products[j] > pivot:
+            j -= 1
+        if i <= j:
+            products[i], products[j] = products[j], products[i]
+            i += 1
+            j -= 1
+    if start < j:
+        qsort_products(products, start, j)
+    if i < stop:
+        qsort_products(products, i, stop)
+    return products
