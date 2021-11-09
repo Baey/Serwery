e3f45d542efd1144008e48a7ec736509ff24d24d
Jakub Szczypek <szczypek@mops.uci.agh.edu.pl>
2021-11-09 16:21:11 +0100

    Jeszcze wincyj testuf

--- servers_test.py
+++ servers_test.py
@@ -30,6 +30,7 @@ class ServerTest(unittest.TestCase):
 
 
 class ClientTest(unittest.TestCase):
+
     def test_total_price_for_normal_execution(self):
         products = [Product('PP234', 2), Product('PP235', 3)]
         for server_type in server_types:
@@ -38,10 +39,16 @@ class ClientTest(unittest.TestCase):
             self.assertEqual(5, client.get_total_price(2))
 
 class ProductTest(unittest.TestCase):
+
     def test_right_name(self):
         product = Product('wod55', 100)
         self.assertEqual(100, product.price)
         self.assertEqual('wod55', product.name)
+
+    def test_wrong_name(self):
+        with self.assertRaises(ValueError):
+            Product('wod55www', 100)
+
 
 
 
